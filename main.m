17315bc273ed7ae366642943f920b98cda0f9818
bartj2 <joelbaer@hotmail.com>
2015-09-28 15:20:22 +0200

    Ändere Kommentare auf Deutsch

--- main.m
+++ main.m
@@ -1,41 +1,47 @@
-% file: main.m
-% 
-% version: see git
-% author: Joel Baertschi [bartj2 or bcj1]
-% description:
-%   This main-file reads in a .jpg image
+% Dateiname: main.m
+% Projekt: Digitalisierung von Anwesenheitslisten.
+% Version: Siehe Git
+% Author: Joel Baertschi [bartj2 or bcj1]
+% Beschreibung:
+%   Diese main-Datei liest ein Bild ein und bereitet es fuer eine
+%   Weiterverarbeitung auf.
 %
 %
 % *************************************************************************
 
-% clean workspace
+% Säubere den Workspace:
 clear all; close all;
 
-% Constants:
+% Konstante Variablen:
 IMAGE_PATH = 'Abwesenheitsliste_Selbstgemacht_Eingescannt_rotated.jpg';
 F = 1; % Figure number
 
-%% *************** read in the image and preprocess it ********************
-% read a .jpg image to the matrix 'Image'
+%% *************** Bild einlesen und vorverarbeiten ***********************
+% Lies Bild ein und speichere es in Matrix Image.
 Image = imread(IMAGE_PATH);
 
-iminfo = imfinfo(IMAGE_PATH);
+iminfo = imfinfo(IMAGE_PATH); % Informationen zum Bild ausgeben
 
-%figure(F);F=F+1;
-%image(Image);
-%axis equal
+figure(F);F=F+1; % neues Fenster öffnen
+image(Image);       % Matrix 'Image' als Bild anzeigen.
+axis equal          % Achsen des Bildes fixieren, um keine Verzerrungen zu erhalten. 
 
 
-% convert image to grayscale
+% Konvertiere das 'Image' in ein Graustufenbild.
 ImageGray = rgb2gray(Image);
-%figure(F);F=F+1;
-%imshow(ImageGray);
+figure(F);F=F+1
+imshow(ImageGray)
 
-
+% Teste die Funktion edge:
 BW1 = edge(ImageGray, 'Canny');
 BW2 = edge(ImageGray, 'Prewitt');
-figure(F);F=F+1;
+figure(F);F=F+1;    % Zeige das Bild nach der Funktion edge
 imshow(BW2);
 
 
-%imshowpair(BW1, BW2, 'montage');
+figure(F);F=F+1;
+histogram(BW2, 20); % Zeige histogramm von BW2
+
+figure(F);F=F+1;
+imshowpair(BW1, BW2, 'montage'); % Vergleiche BW1 und BW2
+
