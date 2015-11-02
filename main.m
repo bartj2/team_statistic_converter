6430018ae8542cc422859a91d18ecc69edc11356
bartj2 <joelbaer@hotmail.com>
2015-11-02 15:52:59 +0100

    Alle Linien gefunden (29). Optimale Parameterwahl für das gewählte Bild

--- main.m
+++ main.m
@@ -33,8 +33,12 @@ ImageGray = rgb2gray(Image);
 %imshow(ImageGray)
 Bin = im2bw(ImageGray);
 Bin = imcomplement(Bin);
-%figure(F);F=F+1;
-%imshow(Bin);
+figure(F);F=F+1;
+imshow(Bin);
+
+% Bin = bwmorph(Bin, 'close', 1);
+% figure(F);F=F+1;
+% imshow(Bin);
 
 % Teste die Funktion edge:
 BW = edge(ImageGray, 'Prewitt');
@@ -44,7 +48,7 @@ BW = edge(ImageGray, 'Prewitt');
 %% Teste Houghtransformation:
 % a) vor edge
 [Hough, Theta, Rho] = hough(Bin, 'RhoResolution', 1, ...
-    'ThetaResolution', 1);
+    'ThetaResolution', 0.1);
 figure(F);F=F+1;
 imshow(imadjust(mat2gray(Hough)), 'XData', Theta, 'YData', Rho,...
     'InitialMagnification', 'fit');
@@ -57,12 +61,12 @@ colorbar;
 
 %max = find(Hough==max(Hough(:)))
 % use houghpeaks:
-Peaks = houghpeaks(Hough, 500, 'Threshold', 0.2*max(Hough(:)), 'NHoodSize', [31, 31]);
+Peaks = houghpeaks(Hough, 500, 'Threshold', 0.2*max(Hough(:)), 'NHoodSize', [51, 51]);
 plot(Theta(Peaks(:,2)),Rho(Peaks(:,1)),'s','color','green');
 hold off
 
 % use houghlines:
-lines = houghlines(Bin, Theta, Rho, Peaks, 'FillGap', 1, 'MinLength', 500);
+lines = houghlines(Bin, Theta, Rho, Peaks, 'FillGap', 5, 'MinLength', 800);
 figure(F); F=F+1;
 image(Image)
 hold on
@@ -70,7 +74,7 @@ axis equal
 n = length(lines)
 for k=1:n
     xy = [lines(k).point1; lines(k).point2];
-    plot(xy(:,1), xy(:,2), 'LineWidth', 1, 'Color', 'blue');
+    plot(xy(:,1), xy(:,2), 'LineWidth', 3, 'Color', 'blue');
 
 end
 hold off
